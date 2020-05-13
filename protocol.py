2e7c4a70c5b9a594e01ab37a84c55e9a71c4e68b
Julian <5569555+jm-@users.noreply.github.com>
2020-05-13 18:53:00 +1200

    add send and recv procedure

--- protocol.py
+++ protocol.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+import struct
 
 import serial
 
@@ -16,7 +17,7 @@ def recv():
         baudrate=17860,
         bytesize=8,
         parity='N',
-        stopbits=1
+        stopbits=2
     )
 
     try:
@@ -35,11 +36,35 @@ def recv():
         b = ser.read(50)
         if b:
             print(f'0x{b.hex()}')
+            data_pkt_length, prog_name_raw, unused0, prog_password_raw = struct.unpack('>H8s8s8s', b[9:35])
+            prog_name = prog_name_raw.rstrip(b'\xff')
+            prog_length = data_pkt_length - 3
+            prog_password = prog_password_raw.rstrip(b'\xff')
+            print(f'DBG: prog_name={prog_name} prog_length={prog_length} prog_password={prog_password}')
 
-
+        # 0x21 means already exists. the calc will ask the user to confirm overwrite
+        #ser.write(b'\x21')
+        # 0x06 means ack, please send data
         ser.write(b'\x06')
 
+        # 0x22 means error (COM/Receive). communication is terminated
 
+        # read the program data
+        b = ser.read(data_pkt_length)
+        if b:
+            print(f'0x{b.hex()}')
+
+        # send another 0x06 ack
+        ser.write(b'\x06')
+
+        # read the end pkt
+        b = ser.read(50)
+        if b[:4] == b':END':
+            print(f'0x{b.hex()}')
+            print('recv complete, exiting')
+            return
+
+        print('idling')
         while True:
             b = ser.read(1)
             if b:
@@ -58,27 +83,86 @@ def send():
         baudrate=17860,
         bytesize=8,
         parity='N',
-        stopbits=1
+        stopbits=2
     )
 
-    print('@ writing 0x16')
-    ser.write(b'\x16')
-
-    b = ser.read(1)
-    if b:
-        print(f'0x{b.hex()}')
-
-    time.sleep(0.01)
-
-    print('@ writing TXT PG packet')
-    ser.write(b':TXT')
-
-    print('idling')
     try:
-        while True:
-            b = ser.read(1)
-            if b:
-                print(f'0x{b.hex()}')
+        print('@ writing 0x16')
+        # 0x16 means request
+        ser.write(b'\x16')
+
+        b = ser.read(1)
+        if b == b'\x13':
+            # 0x13 means ack. we can proceed
+            print(f'0x{b.hex()}')
+        else:
+            print('unexpected, exiting')
+            return
+
+        print('@ writing TXT PG packet')
+        ser.write(
+            b'\x3a\x54\x58\x54\x00\x50\x47\x00\x00\x01\x88\x52\x4f\x55\x4c\x45'
+            b'\x54\x54\x46\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+            b'\xff\xff\xff\x4e\x4c\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+            b'\xff\xed'
+        )
+
+        b = ser.read(1)
+        if b == b'\x06':
+            # 0x06 means ok to start sending
+            print(f'0x{b.hex()}')
+        else:
+            print('unexpected, exiting')
+            return
+
+        print('@ writing program data')
+        ser.write(
+            b'\x3a\x22\x42\x41\x4e\x4b\x22\x3f\x0e\x59\x0d\x59\x0e\x4d\x0d\x31'
+            b'\x0e\x4c\x0d\x31\x0e\x4b\x0d\x31\x0e\x54\x0d\x30\x0e\x57\x0d\x31'
+            b'\x0e\x4a\x0d\x31\x0e\x4e\x0d\x31\x0e\x4c\x0d\xe2\x30\x0d\x30\x0e'
+            b'\x42\x0d\xf7\x0a\x0d\x27\x22\x42\x45\x54\x22\x3f\x0e\x42\x0d\xf7'
+            b'\x00\x4d\x3e\x28\x59\x89\x32\x30\x29\x0d\xf7\x01\x31\x0e\x57\x0d'
+            b'\xf7\x03\x0d\xf7\x00\x42\x3e\x4d\x0d\xf7\x01\x31\x0e\x4a\x0d\x31'
+            b'\x0e\x4b\x0d\x31\x0e\x4c\x0d\xf7\x03\x0d\xf7\x00\x4d\x3c\x31\x0d'
+            b'\xf7\x01\x4e\x89\x31\x0e\x4e\x0d\x59\x0e\x4d\x0d\x31\x0e\x4a\x0d'
+            b'\x31\x0e\x4b\x0d\x31\x0e\x4c\x0d\xf7\x03\x0d\x4c\x0e\x42\x0d\xf7'
+            b'\x0b\x42\x3e\x4d\x7f\xb1\x42\x10\x30\x7f\xb1\x42\x3e\x31\x30\x30'
+            b'\x30\x0d\xde\x33\x37\xc1\x89\x31\x0e\x52\x0d\x4d\x99\x42\x0e\x4d'
+            b'\x0d\x27\xf7\x10\x31\x2c\x31\x2c\x22\x42\x41\x4e\x4b\x3a\x22\x20'
+            b'\x20\x20\x20\x20\x20\x22\x0d\x27\xf7\x10\x37\x2c\x31\x2c\x4d\x0d'
+            b'\x27\x22\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99\x99'
+            b'\x99\x99\x99\x99\x99\x99\x99\x22\x0d\xf7\x00\x52\x10\x31\x32\x0d'
+            b'\xf7\x01\x27\xf7\x10\x31\x2c\x33\x2c\x22\x57\x49\x4e\xab\x20\x22'
+            b'\x0d\x4d\x89\x33\x42\x0e\x4d\x0d\x31\x0e\x4a\x0d\x31\x0e\x4b\x0d'
+            b'\x31\x0e\x4c\x0d\x31\x0e\x42\x0d\xf7\x02\x27\xf7\x10\x31\x2c\x33'
+            b'\x2c\x22\x4c\x4f\x53\x45\xab\x22\x0d\x4a\x89\x4b\x0e\x4c\x0d\x4b'
+            b'\x0e\x4a\x0d\x4c\x0e\x4b\x0d\xf7\x03\x0d\xf7\x10\x31\x2c\x35\x2c'
+            b'\x54\xb9\x4e\x0d\xf7\x04\x31\x0e\x46\xf7\x05\x35\x30\x0d\xf7\x07'
+            b'\x0d\xe2\x35\x0d\xf7\x00\x57\x3d\x31\x0d\xf7\x01\x59\x0e\x4d\x0d'
+            b'\x31\x0e\x4a\x0d\x31\x0e\x4b\x0d\x31\x0e\x4c\x0d\x31\x0e\x42\x0d'
+            b'\x54\x89\x31\x0e\x54\x0d\x4e\x89\x31\x0e\x4e\x0d\x30\x0e\x57\x0d'
+            b'\xf7\x03\x0d\xec\x30\x0d\xff\x89'
+        )
+
+        b = ser.read(1)
+        if b == b'\x06':
+            # 0x06 means ok
+            print(f'0x{b.hex()}')
+        else:
+            print('unexpected, exiting')
+            return
+
+        print('@ writing END packet')
+        ser.write(
+            b'\x3a\x45\x4e\x44\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+            b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+            b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff'
+            b'\xff\x56'
+        )
+
+        print('@ done.')
+        return
+
     except KeyboardInterrupt:
         pass
     finally:
